a59a88b19a6b922633d90efb30427fc6f1d21fba
Tobipeters <oluwatobilobapeterschem@gmail.com>
2024-09-21 15:51:29 +0100

    metadata added

--- src/app/dp-generator/layout.tsx
+++ src/app/dp-generator/layout.tsx
@@ -3,6 +3,7 @@ import '../../styles/index.scss';
 import { Metadata } from 'next';
 import { Footer, Navbar } from '@/components/shared';
 import { DpProvider } from '@/context/dp-context';
+import { SEO } from '@/seo';
 
 export const metadata: Metadata = {
   title: 'DevFest 2024 | DP Generator',
@@ -14,6 +15,12 @@ export const metadata: Metadata = {
 export default function DPGLayout({ children }: { children: React.ReactNode }) {
   return (
     <DpProvider>
+      <SEO
+        title='Generate Your DevFest Lagos 2024 DP'
+        description='Create and Share your DevFest Lagos 2023 DP with friends and on your socials'
+        image='/images/png/dp-opengraph.png'
+      />
+
       <div className=''>
         <Navbar />
         {children}
