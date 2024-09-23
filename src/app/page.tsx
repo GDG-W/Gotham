f8515bac229d3bab0272af63a95667a63d6b12a8
Ayobami Ajayi <lynxdev32@gmail.com>
2024-09-23 16:44:54 +0100

    testing subscribe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Subscribe from '@/components/shared/Subscribe';
 import React from 'react';
 
 const page = () => {
   return (
     <>
       <div>Hello DevFest2024!</div>
+      <Subscribe />
     </>
   );
 };
