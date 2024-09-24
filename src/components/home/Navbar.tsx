2f04fcfe857efb96058fe187265dbdbda621e199
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-09-24 09:27:20 +0100

    made the navbar responsive

--- src/components/home/Navbar.tsx
+++ src/components/home/Navbar.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -13,35 +14,44 @@ const navLinks = [
 
 const Navbar = () => {
   const pathname = usePathname();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const isActive = (path: string) => pathname === path;
 
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   return (
-    <nav className='navbar'>
-      <div className='navbar__background-boxes'>
-        {[...Array(20)].map((_, index) => (
-          <div key={index} className='navbar__background-box' />
-        ))}
+    <nav className={`navbar ${isMenuOpen ? 'menu-open' : ''}`}>
+      <div className='navbar__logo'>
+        <Image src={'/images/svg/devfest--logo.svg'} alt='DevFest Logo' width={137} height={49} />
       </div>
 
-      <div className='navbar__logo'>
-        <Image src={'/images/svg/devfest-logo.svg'} alt='DevFest Logo' width={137} height={49} />
+      <div className='navbar__menu-icon' onClick={toggleMenu}>
+        <Image
+          src={isMenuOpen ? '/images/icons/close-icon.svg' : '/images/icons/menu.svg'}
+          alt={isMenuOpen ? 'Close Menu' : 'Open Menu'}
+          width={30}
+          height={30}
+        />
       </div>
 
       <div className='navbar__links'>
         {navLinks.map((link) => (
-          <Link
-            key={link.href}
-            href={link.href}
-            className={isActive(link.href) ? 'navbar__register-btn' : ''}
-          >
+          <Link key={link.href} href={link.href} className={isActive(link.href) ? 'active' : ''}>
             {link.label}
           </Link>
         ))}
       </div>
+
       <div className='navbar__tickets-btn'>
         <Link href='/get-tickets'>Get Tickets</Link>
-        <Image src={'/images/icons/ticket.svg'} alt='Ticket' width={24} height={24} />
+        <Image
+          className='navbar__tickets-btn__icon'
+          src={'/images/icons/ticket.svg'}
+          alt='Ticket'
+          width={24}
+          height={24}
+        />
       </div>
     </nav>
   );
