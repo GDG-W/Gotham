5e1372fa684e95f085c2f87743721782dc6431ca
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-10-01 08:53:39 +0100

    added layout file

---
+++ src/app/faqs/layout.tsx
@@ -0,0 +1,12 @@
+import FaqNavbar from '@/components/faq/FaqNavbar';
+import React from 'react';
+
+const layout = () => {
+  return (
+    <div>
+      <FaqNavbar />
+    </div>
+  );
+};
+
+export default layout;
