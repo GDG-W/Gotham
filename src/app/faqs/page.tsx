0dcecf90c385cc88e3933ead7334f7b5879941aa
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-10-05 23:52:35 +0100

    updated navbar

--- src/app/faqs/page.tsx
+++ src/app/faqs/page.tsx
@@ -1,11 +1,9 @@
 import Faq from '@/components/faq/Faq';
-import FaqNavbar from '@/components/faq/FaqNavbar';
 import React from 'react';
 
 const page = () => {
   return (
     <div>
-      <FaqNavbar />
       <Faq />
     </div>
   );
