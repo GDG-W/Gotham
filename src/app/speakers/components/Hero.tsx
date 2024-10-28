0ef5e7e4a6846e170300fc0e5b9ab0c4f3a33a7d
Ayobami Ajayi <lynxdev32@gmail.com>
2024-10-28 22:33:04 +0100

    correct speaker obum image

--- src/app/speakers/components/Hero.tsx
+++ src/app/speakers/components/Hero.tsx
@@ -91,7 +91,6 @@ const Hero = () => {
                       fill
                       alt={speaker.name + "'s image"}
                       loading='eager'
-                      priority
                     />
                     <div className={styles.speakerName}>{speaker.name}</div>
                   </li>
@@ -107,13 +106,7 @@ const Hero = () => {
                   className={`${styles.marqueeItem} ${styles.horizontalMarqueeItem}`}
                   key={index}
                 >
-                  <Image
-                    src={speaker.image}
-                    fill
-                    alt={speaker.name + "'s image"}
-                    loading='eager'
-                    priority
-                  />
+                  <Image src={speaker.image} fill alt={speaker.name + "'s image"} loading='eager' />
                   <div className={styles.speakerName}>{speaker.name}</div>
                 </div>
               );
