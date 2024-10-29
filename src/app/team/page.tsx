6662849d7d9ce9b901f85de7b595437f0458d0ec
Ayobami Ajayi <lynxdev32@gmail.com>
2024-10-29 11:14:22 +0100

    add header and footer

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,12 +1,18 @@
 import Hero from './components/Hero';
 import Team from './components/Team';
+import Navbar from '@/components/home/Navbar';
+import Footer from '@/components/home/Footer';
 
 const TeamPage = () => {
   return (
-    <main>
-      <Hero />
-      <Team />
-    </main>
+    <>
+      <Navbar />
+      <main>
+        <Hero />
+        <Team />
+      </main>
+      <Footer />
+    </>
   );
 };
 export default TeamPage;
