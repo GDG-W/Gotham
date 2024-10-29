b135f6e662fb7953b87d80c76d0f4f1cd875e68b
Ayobami Ajayi <lynxdev32@gmail.com>
2024-10-29 10:57:35 +0100

    add header footer and git icon

--- src/app/speakers/page.tsx
+++ src/app/speakers/page.tsx
@@ -2,15 +2,21 @@ import FAQs from '@/components/shared/FAQS';
 import Hero from './components/Hero';
 import Subscribe from '@/components/shared/Subscribe';
 import Speakers from './components/Speakers';
+import Navbar from '@/components/home/Navbar';
+import Footer from '@/components/home/Footer';
 
 const SpeakersPage = () => {
   return (
-    <main className='speakers-main'>
-      <Hero />
-      <Speakers />
-      <FAQs />
-      <Subscribe />
-    </main>
+    <>
+      <Navbar />
+      <main className='speakers-main'>
+        <Hero />
+        <Speakers />
+        <FAQs />
+        <Subscribe />
+      </main>
+      <Footer />
+    </>
   );
 };
 export default SpeakersPage;
