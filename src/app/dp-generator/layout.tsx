b7d9dd9334a6458fad239eb24cf237efdc7ed420
Boluwatife Sowo <Boluwatife.Sowo@vfdgroupltd.com>
2024-10-31 18:56:45 +0100

    updated the metadata openGraph image

--- src/app/dp-generator/layout.tsx
+++ src/app/dp-generator/layout.tsx
@@ -3,25 +3,26 @@ import '../../styles/index.scss';
 import { Metadata } from 'next';
 import { Navbar } from '@/components/shared';
 import { DpProvider } from '@/context/dp-context';
-import { SEO } from '@/seo';
 import Footer from '@/components/home/Footer';
 
+const title = 'DevFest 2024 | DP Generator';
+const description = 'Generator your picture and share on your social media platform';
+
 export const metadata: Metadata = {
-  title: 'DevFest 2024 | DP Generator',
-  description: 'Generator your picture and share on your social media platform',
+  title,
+  description,
   keywords: 'devfest, devfest lagos, coming soon, date, venue',
-  //   metadataBase: new URL('https://devfest-coming-soon.vercel.app/'), // synonymous to the base URL
+  openGraph: {
+    title,
+    description,
+    images: ['/images/png/side-frame.png'],
+    type: 'website',
+  },
 };
 
 export default function DPGLayout({ children }: { children: React.ReactNode }) {
   return (
     <DpProvider>
-      <SEO
-        title='Generate Your DevFest Lagos 2024 DP'
-        description='Create and Share your DevFest Lagos 2023 DP with friends and on your socials'
-        image='/images/png/dp-opengraph.png'
-      />
-
       <div className=''>
         <Navbar />
         {children}
