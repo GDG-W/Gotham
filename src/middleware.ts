9740196b487b95a60ab95eb8ebead367a1feb649
Sodiq Akinjobi <sodiq.akinjobi@gmail.com>
2024-11-13 01:11:58 +0100

    Updated back

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,13 +6,13 @@ export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === '/app') {
     if (/android/i.test(userAgent)) {
       // Redirect to external Android link
-      return NextResponse.redirect('https://android.com');
+      return NextResponse.redirect('https://devfestlagos.com/404');
     } else if (/iphone|ipad|ipod/i.test(userAgent)) {
       // Redirect to external iOS link
-      return NextResponse.redirect('https://ios.com');
+      return NextResponse.redirect('https://apps.apple.com/us/app/devfest-lagos-2024/id6737826901');
     } else {
       // Redirect to a different link for other devices
-      return NextResponse.redirect('https://wikipedia.com');
+      return NextResponse.redirect('https://devfestlagos.com/');
     }
   }
 
